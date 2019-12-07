4d86aa407eac180c6e7c5a192b71987691de7a13
vkirienko <vkirienko@yahoo.com>
2019-12-06 21:53:18 -0500

    Added notes

--- VKirienko.Web/pi-deploy.cmd
+++ VKirienko.Web/pi-deploy.cmd
@@ -1,5 +1,6 @@
 @echo off
 rem Run this
 rem Copy \bin\Release\netcoreapp3.1\linux-arm\publish\ to PI /home/pi/vkirienko/publish
+rem Make sure FileZilla transfers all files including files without extensions as binary files
 
 dotnet publish -c Release -r linux-arm
