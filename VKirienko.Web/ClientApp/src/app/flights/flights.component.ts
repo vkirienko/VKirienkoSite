01d95e67055f852f761f8080b15e0cb448a3b402
vkirienko <vkirienko@yahoo.com>
2020-07-14 19:48:34 -0400

    Added ADSB performance graphs and link to FloghtAware

--- VKirienko.Web/ClientApp/src/app/flights/flights.component.ts
+++ VKirienko.Web/ClientApp/src/app/flights/flights.component.ts
@@ -7,12 +7,46 @@ import { environment } from '../../environments/environment';
   templateUrl: './flights.component.html'
 })
 export class FlightsComponent implements OnInit {
-  url: SafeResourceUrl;
+
+  emptyUrl: SafeResourceUrl;
+
+  urlFlights: SafeResourceUrl;
+  urlGraphs: SafeResourceUrl;
+  urlStats: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
   }
 
   ngOnInit(): void {
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(environment.production ? 'https://vkirienko.com/tar' : 'http://flights.vkirienko.com');
+    this.emptyUrl = this.sanitizer.bypassSecurityTrustResourceUrl('javascript:void(0)');
+
+    this.urlFlights = this.emptyUrl;
+    this.urlGraphs = this.emptyUrl;
+    this.urlStats = this.emptyUrl;
+
+    this.activateTab('flights');
+  }
+
+  activateTab(tab: string): void {
+    switch (tab) {
+      case 'flights':
+        if (this.urlFlights != this.emptyUrl) {
+          return;
+        }
+        this.urlFlights = this.sanitizer.bypassSecurityTrustResourceUrl(environment.production ? 'https://vkirienko.com/tar' : 'https://vkirienko.com/tar');
+        break;
+      case 'graphs':
+        if (this.urlGraphs != this.emptyUrl) {
+          return;
+        }
+        this.urlGraphs = this.sanitizer.bypassSecurityTrustResourceUrl('https://vkirienko.com/graphs1090');
+        break;
+      case 'stats':
+        if (this.urlStats != this.emptyUrl) {
+          return;
+        }
+        this.urlStats = this.sanitizer.bypassSecurityTrustResourceUrl('https://flightaware.com/adsb/stats/user/vkirienko');
+        break;
+    }
   }
 }
