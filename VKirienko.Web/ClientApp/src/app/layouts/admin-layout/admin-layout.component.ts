84b74b58d5261bd8ef70d30a1c3206f70235e985
vkirienko <vkirienko@yahoo.com>
2020-07-14 20:43:35 -0400

    Fixed TS error

--- VKirienko.Web/ClientApp/src/app/layouts/admin-layout/admin-layout.component.ts
+++ VKirienko.Web/ClientApp/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location, PopStateEvent } from '@angular/common';
-import 'rxjs/add/operator/filter';
+import { filter } from 'rxjs/operators';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import PerfectScrollbar from 'perfect-scrollbar';
@@ -15,59 +15,64 @@ export class AdminLayoutComponent implements OnInit {
   private lastPoppedUrl: string;
   private yScrollStack: number[] = [];
 
-  constructor( public location: Location, private router: Router) {}
+  constructor(public location: Location, private router: Router) { }
 
   ngOnInit(): void {
-      const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+    const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-      if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
-          // if we are on windows OS we activate the perfectScrollbar function
+    if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
+      // if we are on windows OS we activate the perfectScrollbar function
+      document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
+    } else {
+      document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+    }
 
-          document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
-      } else {
-          document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+    const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+    const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else
+          window.scrollTo(0, 0);
       }
-      const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
-      const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+    });
 
-      this.location.subscribe((ev:PopStateEvent) => {
-          this.lastPoppedUrl = ev.url;
-      });
-       this.router.events.subscribe((event:any) => {
-          if (event instanceof NavigationStart) {
-             if (event.url != this.lastPoppedUrl)
-                 this.yScrollStack.push(window.scrollY);
-         } else if (event instanceof NavigationEnd) {
-             if (event.url == this.lastPoppedUrl) {
-                 this.lastPoppedUrl = undefined;
-                 window.scrollTo(0, this.yScrollStack.pop());
-             } else
-                 window.scrollTo(0, 0);
-         }
-      });
-      this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
-           elemMainPanel.scrollTop = 0;
-           elemSidebar.scrollTop = 0;
-      });
-      if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
-          let ps = new PerfectScrollbar(elemMainPanel);
-          ps = new PerfectScrollbar(elemSidebar);
-      }
+    this._router = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      elemMainPanel.scrollTop = 0;
+      elemSidebar.scrollTop = 0;
+    });
+
+    if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+      let ps = new PerfectScrollbar(elemMainPanel);
+      ps = new PerfectScrollbar(elemSidebar);
+    }
   }
 
   ngAfterViewInit(): void {
-      this.runOnRouteChange();
+    this.runOnRouteChange();
   }
 
-  isMaps(path): boolean{
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-          return false;
-      }
-      else {
-          return true;
-      }
+  isMaps(path): boolean {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    titlee = titlee.slice(1);
+    if (path == titlee) {
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
   runOnRouteChange(): void {
@@ -79,11 +84,10 @@ export class AdminLayoutComponent implements OnInit {
   }
 
   isMac(): boolean {
-      let bool = false;
-      if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
-          bool = true;
-      }
-      return bool;
+    let bool = false;
+    if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+      bool = true;
+    }
+    return bool;
   }
-
 }
