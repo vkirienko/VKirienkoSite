f82daf0059da612be394093fc8c8ebfeccb1349c
Vlad Kirienko <vkirienko@yahoo.com>
2020-11-13 10:42:45 -0500

    added systemctl daemon-reload

--- VKirienko.Web/install.sh
+++ VKirienko.Web/install.sh
@@ -12,6 +12,7 @@ systemctl stop vkirienko-web.service
 wget -O /tmp/vkirienko-web.service https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/VKirienko.Web/vkirienko-web.service
 cp /tmp/vkirienko-web.service /etc/systemd/system
 
+systemctl daemon-reload
 systemctl start vkirienko-web.service
 
 echo "Restarting nginx ..."
