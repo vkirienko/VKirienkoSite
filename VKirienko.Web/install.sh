06b1e5f63b82e290ea3dcbc0ae5d884badce6f07
Vlad Kirienko <vkirienko@yahoo.com>
2020-11-13 10:22:43 -0500

    vkirienko-web.service deployment

--- VKirienko.Web/install.sh
+++ VKirienko.Web/install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -e
+trap 'echo ERROR on line number $LINENO' ERR
+
 echo "Setting execute permission to VKirienko.Web ..."
 chmod +x /home/vlad/web/publish/VKirienko.Web
 
-echo "Restarting vkirienko-web ..."
-systemctl restart vkirienko-web.service
+echo "Deploying vkirienko-web service ..."
+systemctl stop vkirienko-web.service
+
+wget -O /tmp/vkirienko-web.service https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/vkirienko-web.service
+cp /tmp/vkirienko-web.service /etc/systemd/system
+
+systemctl start vkirienko-web.service
 
 echo "Restarting nginx ..."
 systemctl restart nginx
