c91bafc3a7ab140d98a6154cab393df841085c5b
Vlad Kirienko <vkirienko@yahoo.com>
2021-02-03 09:50:00 -0500

    Cleanup, fixed deployment minification problem

--- VKirienko.Web/deploy.ps1
+++ VKirienko.Web/deploy.ps1
@@ -7,7 +7,14 @@ $key = (Get-Content -path ..\..\Raspberry\google\GoogleMapsApiKey.txt -Raw)
 
 Write-Host $key
 
-Get-ChildItem '.\bin\Release\net5.0\linux-arm\publish\ClientApp\dist\*.*' -Recurse | ForEach {
+Get-ChildItem '.\bin\Release\net5.0\linux-arm\publish\ClientApp\dist\*.js' -Recurse | ForEach {
+	Write-Host $_
+
+	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $key)
+	[System.IO.File]::WriteAllText($_, $content)
+}
+
+Get-ChildItem '.\bin\Release\net5.0\linux-arm\publish\ClientApp\dist\*.html' -Recurse | ForEach {
 	Write-Host $_
 
 	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $key)
