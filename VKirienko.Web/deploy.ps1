4f335e1e9aa6cdbdcaffeee3e53750cd11786825
Vlad Kirienko <vkirienko@yahoo.com>
2021-11-09 22:49:10 -0500

    Updated build for ,NET 6 (remove warning)

--- VKirienko.Web/deploy.ps1
+++ VKirienko.Web/deploy.ps1
@@ -1,20 +1,20 @@
 & cmd /c rmdir bin /s /q
 & cmd /c rmdir obj /s /q
 
-& dotnet publish -c Release -r linux-arm
+& dotnet publish -c Release -r linux-arm --sc
 
 $key = (Get-Content -path ..\..\Raspberry\google\GoogleMapsApiKey.txt -Raw)
 
 Write-Host $key
 
-Get-ChildItem '.\bin\Release\net5.0\linux-arm\publish\ClientApp\dist\*.js' -Recurse | ForEach {
+Get-ChildItem '.\bin\Release\net6.0\linux-arm\publish\ClientApp\dist\*.js' -Recurse | ForEach {
 	Write-Host $_
 
 	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $key)
 	[System.IO.File]::WriteAllText($_, $content)
 }
 
-Get-ChildItem '.\bin\Release\net5.0\linux-arm\publish\ClientApp\dist\*.html' -Recurse | ForEach {
+Get-ChildItem '.\bin\Release\net6.0\linux-arm\publish\ClientApp\dist\*.html' -Recurse | ForEach {
 	Write-Host $_
 
 	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $key)
