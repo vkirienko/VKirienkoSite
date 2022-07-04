b5dce03c29c4083c667b39abf0c3836a828e336d
Vlad Kirienko <vkirienko@yahoo.com>
2022-07-03 20:28:53 -0400

    Updated packages + config changes

--- VKirienko.Web/ClientApp/e2e/app.e2e-spec.ts
+++ VKirienko.Web/ClientApp/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { MaterialDashboardAngularPage } from './app.po';
 
-describe('material-dashboard-angular App', () => {
+describe('vkirienko-site App', () => {
   let page: MaterialDashboardAngularPage;
 
   beforeEach(() => {
