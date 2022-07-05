acce456447ae12de338a2faf221842deef1ae505
Vlad Kirienko <vkirienko@yahoo.com>
2022-07-04 23:10:15 -0400

    Replace key in html file only

--- VKirienko.Web/deploy.ps1
+++ VKirienko.Web/deploy.ps1
@@ -7,13 +7,6 @@ $key = (Get-Content -path ..\..\Raspberry\google\GoogleMapsApiKey.txt -Raw)
 
 Write-Host $key
 
-Get-ChildItem '.\bin\Release\net6.0\linux-arm\publish\ClientApp\dist\*.js' -Recurse | ForEach {
-	Write-Host $_
-
-	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $key)
-	[System.IO.File]::WriteAllText($_, $content)
-}
-
 Get-ChildItem '.\bin\Release\net6.0\linux-arm\publish\ClientApp\dist\*.html' -Recurse | ForEach {
 	Write-Host $_
 
