6ceff068ee0cd656df684f9165c217c9e2d8775c
Vlad Kirienko <vkirienko@yahoo.com>
2022-07-20 19:41:11 -0400

    Stop nginx before deployment

--- VKirienko.Web/install.sh
+++ VKirienko.Web/install.sh
@@ -7,14 +7,16 @@ echo "Setting execute permission to VKirienko.Web ..."
 chmod +x /home/vlad/web/publish/VKirienko.Web
 
 echo "Deploying vkirienko-web service ..."
+systemctl stop nginx
 systemctl stop vkirienko-web.service
 
 wget -O /tmp/vkirienko-web.service https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/VKirienko.Web/vkirienko-web.service
 cp /tmp/vkirienko-web.service /etc/systemd/system
 
+echo "Starting web site service ..."
 systemctl daemon-reload
 systemctl start vkirienko-web.service
 
-echo "Restarting nginx ..."
-systemctl restart nginx
+echo "Starting nginx ..."
+systemctl start nginx
 
