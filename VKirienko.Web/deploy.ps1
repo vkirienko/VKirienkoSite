b179adcf39e7b4906f1dabe89892587456b6155e
Vlad Kirienko <vkirienko@yahoo.com>
2023-11-14 22:11:04 -0500

    Fixed build script

--- VKirienko.Web/deploy.ps1
+++ VKirienko.Web/deploy.ps1
@@ -9,7 +9,7 @@ $googleMapsApiKey = (Get-Content -path ..\..\Raspberry\google\GoogleMapsApiKey.t
 Write-Host ""
 Write-Host $googleMapsApiKey
 
-Get-ChildItem '.\bin\Release\net8.0\linux-arm64\publish\ClientApp\dist\*.html' -Recurse | ForEach {
+Get-ChildItem '.\bin\Release\net8.0\linux-arm64\publish\wwwroot\browser\*.html' -Recurse | ForEach {
 	Write-Host $_
 
 	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_GOOGLE_MAPS_API_KEY", $googleMapsApiKey)
@@ -22,7 +22,7 @@ $appInsightsConnectionString = (Get-Content -path ..\..\Raspberry\app-secrets\ap
 Write-Host ""
 Write-Host $appInsightsConnectionString
 
-Get-ChildItem '.\bin\Release\net8.0\linux-arm64\publish\ClientApp\dist\*.js' -Recurse | ForEach {
+Get-ChildItem '.\bin\Release\net8.0\linux-arm64\publish\wwwroot\browser\*.js' -Recurse | ForEach {
 	Write-Host $_
 
 	$content = [System.IO.File]::ReadAllText($_).Replace("YOUR_APP_INSIGHTS_CONNECTION_STRING", $appInsightsConnectionString)
