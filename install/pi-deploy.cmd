74bcb22ec577e7c8f377a5c6e5d279270b699ab0
Vlad Kirienko <vkirienko@yahoo.com>
2023-11-17 09:39:28 -0500

    Fixed production build

--- install/pi-deploy.cmd
+++ install/pi-deploy.cmd
@@ -9,7 +9,7 @@ rem
 rem 3. Copy \bin\Release\net8.0\linux-arm\publish\ to RPi /home/vlad/web/publish 
 rem    (make sure FileZilla transfers all files including files without extensions as binary files)
 rem
-rem 4. sudo bash -c "$(wget -q -O - https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/VKirienko.Web/install.sh)"
+rem 4. sudo bash -c "$(wget -q -O - https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/install/install.sh)"
 rem
 
 powershell.exe -f deploy.ps1 
