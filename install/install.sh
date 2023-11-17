48d042fbbd668deee76172ad680ef390f26bf15b
Vlad Kirienko <vkirienko@yahoo.com>
2023-11-17 06:48:39 -0500

    Fixed path

--- install/install.sh
+++ install/install.sh
@@ -10,7 +10,7 @@ echo "Deploying vkirienko-web service ..."
 systemctl stop nginx
 systemctl stop vkirienko-web.service
 
-wget -O /tmp/vkirienko-web.service https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/VKirienko.Web/vkirienko-web.service
+wget -O /tmp/vkirienko-web.service https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/install/vkirienko-web.service
 cp /tmp/vkirienko-web.service /etc/systemd/system
 
 echo "Starting web site service ..."
