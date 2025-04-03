acdeae40c3c543ef0e0670001ff2fb85c71869aa
Vlad Kirienko <12551258+vkirienko@users.noreply.github.com>
2025-04-02 21:48:07 -0400

    .NET 9 and Angular 19 upgrade

--- install/deploy2.ps1
+++ install/deploy2.ps1
@@ -5,7 +5,7 @@ $session = New-PSSession -HostName 10.44.33.59 -UserName vlad
 
 #Remove-Item /home/vlad/web/publish/*.* -Recurse -Verbose
 
-Copy-Item ".\bin\Release\net8.0\linux-arm64\publish" -Destination "/home/vlad/web" -Force -Recurse -Verbose -ToSession $session 
+Copy-Item ".\bin\Release\net9.0\linux-arm64\publish" -Destination "/home/vlad/web" -Force -Recurse -Verbose -ToSession $session 
 
 #Invoke-Command -Session $session -ScriptBlock { & bash -c "$(wget -q -O - https://raw.githubusercontent.com/vkirienko/VKirienkoSite/master/VKirienko.Web/install.sh)"} -Verbose
 
